bc17a2c7a4526b3aae72cf9eaed5140d7d6ee1ea
toantinblockchain <toantinblockchain@gmail.com>
2025-06-15 07:32:22 +0700

    Update run_node.py

--- run_node.py
+++ run_node.py
@@ -9,7 +9,7 @@ from cryptography.hazmat.primitives import serialization, hashes
 from cryptography import x509
 from cryptography.x509.oid import NameOID
 from cryptography.hazmat.backends import default_backend
-from api_server_quart import run_api
+from api_server_quart import initialize_quart_globals
 import requests
 
 
